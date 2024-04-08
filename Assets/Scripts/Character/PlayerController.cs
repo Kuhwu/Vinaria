da0b8687cf41fc179b472a5eb37ccb3f88196f7a
Kawu <138832632+Kawudesu@users.noreply.github.com>
2024-04-08 22:24:03 +0800

    New Movement

--- Assets/Scripts/Character/PlayerController.cs
+++ Assets/Scripts/Character/PlayerController.cs
@@ -1,44 +1,58 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    private Vector3 _input;
-    private bool _forwardPressed;
+    [SerializeField] private float _speed = 5f;
 
-    [SerializeField] private Rigidbody _rb;
-    [SerializeField] private float _speed = 5;
+    private Camera _mainCamera;
+    private bool _isMoving;
+    private Vector3 _targetPosition;
+
+    void Start()
+    {
+        _mainCamera = Camera.main;
+    }
 
     void Update()
     {
-        GatherInput();
-        Look();
-    }
-
-    void FixedUpdate()
-    {
-        Move();
-    }
-    
-    void GatherInput()
-    {
-        _input = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical"));
-        _forwardPressed = Input.GetKey(KeyCode.LeftShift);
-    }
-
-    void Look()
-    {
-        var relative = (transform.position + _input) - transform.position;
-        var rot = Quaternion.LookRotation(relative, Vector3.up);
-        transform.rotation = rot;
-    }
-
-    void Move()
-    {
-        if (_forwardPressed)
+        if (Input.GetMouseButtonDown(1)) // Right mouse button is clicked
         {
-            _rb.MovePosition(transform.position + transform.forward * _speed * Time.deltaTime); //LSHIFT move forward
+            SetTargetPosition();
         }
-    }   
+
+        if (_isMoving)
+        {
+            MoveToTargetPosition();
+        }
+    }
+
+    void SetTargetPosition()
+    {
+        Ray ray = _mainCamera.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+
+        if (Physics.Raycast(ray, out hit))
+        {
+            _targetPosition = hit.point;
+            _targetPosition.y = transform.position.y; // Keep the same height as the player
+            _isMoving = true;
+        }
+    }
+
+    void MoveToTargetPosition()
+    {
+        // Calculate the direction to move towards the target position
+        Vector3 direction = (_targetPosition - transform.position).normalized;
+        transform.position += direction * _speed * Time.deltaTime;
+
+        // Rotate towards the target point (optional)
+        Quaternion targetRotation = Quaternion.LookRotation(direction);
+        transform.rotation = Quaternion.RotateTowards(transform.rotation, targetRotation, 360f * Time.deltaTime);
+
+        // If the player is close enough to the target position, stop moving
+        if (Vector3.Distance(transform.position, _targetPosition) < 0.1f)
+        {
+            _isMoving = false;
+        }
+    }
 }
