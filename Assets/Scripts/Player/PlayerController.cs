7e40604a601946b9ead4d51984583b712625611d
Kawu <138832632+Kawudesu@users.noreply.github.com>
2024-11-24 00:44:33 +0800

    MOVEMENT UPDATE
    new point and click movement

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -2,38 +2,72 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    [SerializeField] private Rigidbody _rb;
+    [SerializeField] private float _speed = 5;
+    [SerializeField] private float _turnSpeed = 360;
+    [SerializeField] private LayerMask _groundLayer; // Define which layer represents the ground
+    private Vector3 _targetPosition;
+    private bool _hasTarget = false;
 
-    [SerializeField] private Rigidbody _rb;   
-    [SerializeField] private float _speed = 5;  
-    [SerializeField] private float _turnSpeed = 360;    
-    private Vector3 _input;
-
-    void Update(){
-        GatherInput();
+    void Update()
+    {
+        HandleClickInput();
         Look();
     }
 
-    void FixedUpdate(){
+    void FixedUpdate()
+    {
         Move();
     }
 
-    void GatherInput(){
-        _input = new Vector3(Input.GetAxisRaw("Horizontal"),0,Input.GetAxisRaw("Vertical"));
+    void HandleClickInput()
+    {
+        if (Input.GetMouseButtonDown(1)) // Right mouse button
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
 
-    }
-
-    void Look(){
-
-        if(_input != Vector3.zero){
-            var relative = (transform.position + _input) - transform.position;
-            var rot = Quaternion.LookRotation(relative,Vector3.up);
-
-            transform.rotation = Quaternion.RotateTowards(transform.rotation,rot,_turnSpeed * Time.deltaTime);
+            if (Physics.Raycast(ray, out RaycastHit hit, Mathf.Infinity, _groundLayer))
+            {
+                _targetPosition = hit.point; // Set the target position
+                _hasTarget = true;
+            }
         }
     }
 
-    void Move(){
-        _rb.MovePosition(transform.position + (transform.forward * _input.magnitude) * _speed * Time.deltaTime);
+    void Look()
+    {
+        if (_hasTarget)
+        {
+            Vector3 direction = (_targetPosition - transform.position).normalized;
+            direction.y = 0; // Ignore vertical difference for rotation
+
+            if (direction != Vector3.zero)
+            {
+                var rot = Quaternion.LookRotation(direction, Vector3.up);
+                transform.rotation = Quaternion.RotateTowards(transform.rotation, rot, _turnSpeed * Time.deltaTime);
+            }
+        }
     }
 
+    void Move()
+    {
+        if (_hasTarget)
+        {
+            Vector3 direction = (_targetPosition - transform.position).normalized;
+            direction.y = 0; // Ignore vertical movement
+
+            // Move the player towards the target position
+            Vector3 newPosition = transform.position + direction * _speed * Time.deltaTime;
+
+            // Stop moving if the player is very close to the target
+            if (Vector3.Distance(transform.position, _targetPosition) <= 0.1f)
+            {
+                _hasTarget = false;
+            }
+            else
+            {
+                _rb.MovePosition(newPosition);
+            }
+        }
+    }
 }
